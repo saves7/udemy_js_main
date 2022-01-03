4b06cce5f2da94723b93bdc84feec9d57c1d785f
saves7 <a_v.efimov@mail.ru>
2022-01-03 18:03:02 +0500

    callback in work

--- callback_function.js
+++ callback_function.js
@@ -12,3 +12,20 @@ first();
 second();
 
 //callback функция - это функция, которая выполняется после выполнения другой функции.
+//в одну функцию в качестве аргумента передается другая функция, что обеспечивает выполнение функций в котролируемом порядке
+
+function learnLang (lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+function done () {
+    console.log('Я изучил этот урок');
+}
+
+learnLang ('Javascript', done);
+
+
+/*learnLang ('Javascript', function(){
+    console.log('Я прошел этот урок');  
+});*/
