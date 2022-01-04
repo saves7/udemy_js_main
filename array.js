e165586bae347756d3a471fba44499059778d023
saves7 <a_v.efimov@mail.ru>
2022-01-04 17:37:39 +0500

    добавлено описание популярных методов массива

--- array.js
+++ array.js
@@ -27,10 +27,28 @@ for (let value of arr) {
 
 //в переборке с помощью forEach не доступны break и continue, но очень удобна,
 //в for each используется функция обратного вызова, т.е функция вызывается при обращении к элементу
+//1-й аргумент функции это элемент массива, 2-й аргумент порядковый номер, 3-й сам массив
 
-arr.forEach(function(){
-
+arr.forEach(function(item){
+    console.log(item);
 });
 
+arr.forEach(function(item, i){
+    console.log(`Элемент ${i} имеет значение ${item}`);
+});
 
+arr.forEach(function(item, i){
+    console.log(`Элемент ${i} имеет значение ${item} в массиве ${arr}`);
+});
 
+// так же есть переборка массива с трансформацией, это такие методы
+//arr.map  arr.filter   arr.reduce  arr.every/some
+
+//у массива также есть методы
+
+//arr.split(s) - превращает строку в массив, s - разделитель
+//arr.join(s) - превращает массив в строку, s - разделитель
+//arr.sort(fn) - сортирует массив, если не передать функцию сравнения, то сортирует элементы как строки
+//arr.slice(begin, end);
+//arr.reverse();
+//arr.concat(item1...);
