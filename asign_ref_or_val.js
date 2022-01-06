5e8ea24a04e6954377fb0db88c41ca3cf792ee5a
saves7 <a_v.efimov@mail.ru>
2022-01-06 16:15:33 +0500

    Присвоение по значению и по ссылке, методы копирования объктов и массивов

--- asign_ref_or_val.js
+++ asign_ref_or_val.js
@@ -3,13 +3,13 @@
 let a = 5,
     b = a;
 
-    b = b + 1;
+b = b + 1;
 
-console.log (a,b); //5 и 6 переменная a не изменилась
+console.log(a, b); //5 и 6 переменная a не изменилась
 
 
 let arr1 = [5, 7, 6, 4];
-let arr2 =[];
+let arr2 = [];
 //arr2[0] = 9;
 //console.log(arr1); //[ 9, 7, 6, 4 ] - хоть мы и меняли копию массива мы изменили оригинал,
 //так как второй массив получен путем ссылки на первый, то через эту ссылку добрались до ориганала.
@@ -18,11 +18,11 @@ let arr2 =[];
 //или через специальные методы,
 //но все это будет поверхностное копирование, не затрагивающее копирование вложенных свойств, которые по прежнему будут копироваться через ссылку.
 
-for(let i = 0; i<arr1.length; i++) {
-    arr2[i]=arr1[i];
+for (let i = 0; i < arr1.length; i++) {
+    arr2[i] = arr1[i];
 
 }
-arr2[0] =9;
+arr2[0] = 9;
 console.log(arr1); //[ 5, 7, 6, 4 ] теперь arr1 не меняется хоть и изменили arr2
 console.log(arr2); //[ 9, 7, 6, 4 ]
 
@@ -38,15 +38,15 @@ let obj1 = {
 //теперь меняя obj2 мы меняем obj1
 
 //obj2.a = 1;
-console.log (obj1); //свойство a стало 1, вместо 5;
+console.log(obj1); //свойство a стало 1, вместо 5;
 
 //объекты удобно перебирать с помошью for in, и перебирая создавать копию 
 
-function doObjcopy(mainObj){
+function doObjcopy(mainObj) {
     let objCopy = {};
     let key;
-    for(key in mainObj) {
-        objCopy[key]=mainObj[key];
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
     }
     return objCopy;
 }
@@ -60,11 +60,11 @@ console.log(obj2); //{ a: 77, b: 'какое то свойство', c: 2 }
 //еще пример, объект со вложенным объектом, который показывает что копирование поверхностное
 
 let nunmbers = {
-    a : 5,
-    b : 9,
-    c : {
-        x : 4,
-        y : 33,
+    a: 5,
+    b: 9,
+    c: {
+        x: 4,
+        y: 33,
     }
 };
 
@@ -77,7 +77,7 @@ console.log(copyNumbers); //{ a: 7, b: 9, c: { x: 77, y: 33 } }
 
 //по мимо переборки свойств и значений одного объекта с подстановкой в другой объект для копирование есть еще способы:
 //Object.assign(mainObj, twoObj); Т.е помести в mainObj другой объект twoObj
-//Т.е. 
+
 
 let enotherNumbers = {
     a1: 4,
@@ -85,9 +85,34 @@ let enotherNumbers = {
     a3: 22
 };
 
-let joinNumbers = Object.assign(nunmbers,enotherNumbers);
+let joinNumbers = Object.assign(nunmbers, enotherNumbers);
 console.log(joinNumbers);
 
 
+//Object.assign({}, twoObj)Т.е. подставив одним из аргументов пусой объект получим копию другого объекта указанного как аргумент
 
+let enotherNumbers2 = Object.assign({}, enotherNumbers);
+console.log(enotherNumbers2);
 
+//Методы поверхностного копирования массивов:
+//массивы можно копировать с помощью метода slice: arr.slice([begin[, end]]), если без параметров то скопируется весь массив
+let Array1 = [0, 2, 4, 8];
+let copyArra1 = Array1.slice();
+console.log(copyArra1);
+
+//Еще одним методом можно копировать как объекты так массивы, это spred - развертывание, обозначается . . . 
+
+let testObj = {
+    a: '1',
+    b: '2',
+    c: '3'
+};
+
+let copyTestObj = { ...testObj };
+console.log(copyTestObj);
+
+//Так же метод . . . - spred можно использовать для передачи параметров:
+
+function log(a, b, c) {
+    console.log(a);
+}
