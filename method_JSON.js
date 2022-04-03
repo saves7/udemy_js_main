c8d59a9b7c424c96a84e1914a5be2c82232b67da
saves7 <a_v.efimov@mail.ru>
2022-04-03 15:39:28 +0500

    Clone object

--- method_JSON.js
+++ method_JSON.js
@@ -1,9 +1,22 @@
 let objCapital = {
     Gb: 'London',
     USA: 'Washington',
-    Rus: 'Moscow'
+    Rus: 'Moscow',
+    republicsRus: {
+        capBashk: 'Ufa',
+        capTatr: 'Kazan'
+    }
 
 };
 
-let jsn = JSON.stringify(objCapital);
+let jsn = JSON.stringify(objCapital);   // получить из объекта JSON формат
 console.log(jsn);
+
+//из JSON формата получть объект  JSON.parse
+
+let objFromJSON = JSON.parse(jsn);
+console.log(objFromJSON); 
+
+//с помощью методов parse и stringify создаются глубокие копии объектов
+
+let clone = JSON.parse(JSON.stringify(objCapital));  // сначала получили json формат, а затем из него глубокую копию;
